d2d968ab24f870b4fbc421a47cea551447049135
krisna dwi setyaadi <krisnadwisetyadi@gmail.com>
2024-03-25 00:27:40 +0700

    first commit

---
+++ controllers/categorycontroller/categorycontroller.go
@@ -0,0 +1,57 @@
+package categorycontroller
+
+import (
+	"net/http"
+	"text/template"
+	"time"
+	"todos/entities"
+	"todos/models/categorymodel"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	categories := categorymodel.GetAll()
+	data := map[string]any{
+		"categories": categories,
+	}
+
+	temp, err := template.ParseFiles("views/category/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	temp.Execute(w, data)
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/create.html")
+		if err != nil {
+			panic(err)
+		}
+
+		temp.Execute(w, nil)
+	}
+
+	if r.Method == "POST" {
+		var category entities.Category
+
+		category.Name = r.FormValue("name")
+		category.CreatedAt = time.Now()
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Create(category); !ok {
+			temp, _ := template.ParseFiles("views/category/create.html")
+			temp.Execute(w, nil)
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ models/categorymodel/categorymodel.go
@@ -0,0 +1,49 @@
+package categorymodel
+
+import (
+	"todos/config"
+	"todos/entities"
+)
+
+func GetAll() []entities.Category {
+	rows, err := config.DB.Query(`SELECT * FROM categories`)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	var categories []entities.Category
+
+	for rows.Next() {
+		var category entities.Category
+		// scan memindahkan value rows ke category
+		if err := rows.Scan(&category.Id, &category.Name, &category.CreatedAt, &category.UpdatedAt); err != nil {
+			panic(err)
+		}
+
+		categories = append(categories, category)
+	}
+
+	return categories
+}
+
+func Create(category entities.Category) bool {
+	result, err := config.DB.Exec(`
+		INSERT INTO categories 	(name, created_at, updated_at)
+		VALUE (?, ?, ?)
+	`, category.Name, category.CreatedAt, category.UpdatedAt)
+
+	if err != nil {
+		panic(err)
+	}
+
+	lastInsertId, err := result.LastInsertId()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return lastInsertId > 0
+}
